337a4a3bbe594e338ea92f2b1c88c5ced5880cfa
Boyan Buliev <boyanbot@gmail.com>
2023-10-30 21:43:43 +0200

    removed ArrayList import

--- OOP/Workshop 2/src/com/company/oop/cosmetics/commands/CreateToothpasteCommand.java
+++ OOP/Workshop 2/src/com/company/oop/cosmetics/commands/CreateToothpasteCommand.java
@@ -6,6 +6,7 @@ import com.company.oop.cosmetics.models.enums.GenderType;
 import com.company.oop.cosmetics.utils.ParsingHelpers;
 import com.company.oop.cosmetics.utils.ValidationHelpers;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
