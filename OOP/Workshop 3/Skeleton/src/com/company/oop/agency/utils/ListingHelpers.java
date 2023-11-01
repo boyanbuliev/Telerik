43758a81efb3d58a5b3741b71a9e4909d0b19872
Boyan Buliev <boyanbot@gmail.com>
2023-11-01 19:02:34 +0200

    finished workshop 3 task

--- OOP/Workshop 3/Skeleton/src/com/company/oop/agency/utils/ListingHelpers.java
+++ OOP/Workshop 3/Skeleton/src/com/company/oop/agency/utils/ListingHelpers.java
@@ -2,42 +2,30 @@ package com.company.oop.agency.utils;
 
 import com.company.oop.agency.commands.CommandsConstants;
 import com.company.oop.agency.models.contracts.Journey;
+import com.company.oop.agency.models.contracts.Printable;
 import com.company.oop.agency.models.contracts.Ticket;
 import com.company.oop.agency.models.vehicles.contracts.Vehicle;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class ListingHelpers {
 
     public static String ticketsToString(List<Ticket> tickets) {
-        List<String> result = new ArrayList<>();
-        for (Ticket ticket : tickets) {
-            result.add(ticket.getAsString());
-        }
-
-        return String.join(CommandsConstants.JOIN_DELIMITER + System.lineSeparator(), result).trim();
+        return elementsToString(tickets);
     }
 
     public static String vehiclesToString(List<Vehicle> vehicles) {
-        List<String> result = new ArrayList<>();
-        for (Vehicle vehicle : vehicles) {
-            result.add(vehicle.getAsString());
-        }
-
-        return String.join(CommandsConstants.JOIN_DELIMITER + System.lineSeparator(), result).trim();
+        return elementsToString(vehicles);
     }
 
     public static String journeysToString(List<Journey> journeys) {
-        List<String> result = new ArrayList<>();
-        for (Journey journey : journeys) {
-            result.add(journey.getAsString());
-        }
-
-        return String.join(CommandsConstants.JOIN_DELIMITER + System.lineSeparator(), result).trim();
+        return elementsToString(journeys);
     }
 
-//    public static <T extends Printable> String elementsToString(List<T> elements) {
-//    }
+    public static <T extends Printable> String elementsToString(List<T> elements) {
+        return elements.stream().map(Printable::getAsString)
+                .collect(Collectors.joining(CommandsConstants.JOIN_DELIMITER + System.lineSeparator())).trim();
+    }
 
 }
