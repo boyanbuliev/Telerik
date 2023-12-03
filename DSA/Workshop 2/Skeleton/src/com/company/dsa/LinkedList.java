64d862e03e7b727522aa7259071980a58cb0849f
Boyan Buliev <boyanbot@gmail.com>
2023-12-03 17:00:16 +0200

    Finished LinkedList task

--- DSA/Workshop 2/Skeleton/src/com/company/dsa/LinkedList.java
+++ DSA/Workshop 2/Skeleton/src/com/company/dsa/LinkedList.java
@@ -1,6 +1,7 @@
 package com.company.dsa;
 
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 public class LinkedList<T> implements List<T> {
     private Node head;
@@ -16,57 +17,152 @@ public class LinkedList<T> implements List<T> {
 
     @Override
     public void addFirst(T value) {
-        throw new UnsupportedOperationException();
+        Node curr = new Node(value);
+        if (size == 0) {
+            head = curr;
+            tail = curr;
+        } else {
+            curr.next = head;
+            head = curr;
+        }
+        size++;
     }
 
     @Override
     public void addLast(T value) {
-        throw new UnsupportedOperationException();
+        Node curr = new Node(value);
+        if (size == 0) {
+            head = curr;
+        } else {
+            tail.next = curr;
+            curr.prev = tail;
+        }
+        tail = curr;
+        size++;
     }
 
     @Override
     public void add(int index, T value) {
-        throw new UnsupportedOperationException();
+        if (index == size) {
+            addLast(value);
+            return;
+        }
+        checkIndex(index);
+        Node prevNode = head;
+        for (int i = 0; i < index - 1; i++) {
+            prevNode = prevNode.next;
+        }
+        Node newNode = new Node(value);
+        Node nextNode = prevNode.next;
+        prevNode.next = newNode;
+        nextNode.prev = newNode;
+        newNode.prev = prevNode;
+        newNode.next = nextNode;
+        size++;
     }
 
     @Override
     public T getFirst() {
-        throw new UnsupportedOperationException();
+        checkIfEmpty();
+        return head.value;
     }
 
     @Override
     public T getLast() {
-        throw new UnsupportedOperationException();
+        checkIfEmpty();
+        return tail.value;
     }
 
     @Override
     public T get(int index) {
-        throw new UnsupportedOperationException();
+        checkIndex(index);
+        checkIfEmpty();
+        Node curr = head;
+        for (int i = 0; i < index; i++) {
+            curr = curr.next;
+        }
+        return curr.value;
     }
 
     @Override
     public int indexOf(T value) {
-        throw new UnsupportedOperationException();
+        int index = 0;
+        for (Node curr = head; curr != null; curr = curr.next) {
+            if (curr.value == value)
+                return index;
+            index++;
+        }
+        return -1;
     }
 
     @Override
     public T removeFirst() {
-        throw new UnsupportedOperationException();
+        checkIfEmpty();
+        Node curr = head;
+        if (size == 1) {
+            head = null;
+        } else {
+            head = head.next;
+            head.prev = null;
+        }
+        size--;
+        return curr.value;
     }
 
     @Override
     public T removeLast() {
-        throw new UnsupportedOperationException();
+        checkIfEmpty();
+        Node curr = tail;
+        if (size == 1) {
+            tail = null;
+        } else {
+            tail = tail.prev;
+            tail.next = null;
+        }
+        size--;
+        return curr.value;
     }
 
     @Override
     public int size() {
-        throw new UnsupportedOperationException();
+        return size;
     }
 
     @Override
     public Iterator<T> iterator() {
-        throw new UnsupportedOperationException();
+        return new ListItr();
+    }
+
+    private class ListItr implements Iterator<T> {
+        private Node curr;
+
+        ListItr() {
+            curr = head;
+        }
+
+        @Override
+        public boolean hasNext() {
+            return curr != null;
+        }
+
+        @Override
+        public T next() {
+            T value = curr.value;
+            curr = curr.next;
+            return value;
+        }
+    }
+
+    private void checkIndex(int index) {
+        if (index > size || index < 0) {
+            throw new NoSuchElementException();
+        }
+    }
+
+    private void checkIfEmpty() {
+        if (size == 0) {
+            throw new NoSuchElementException();
+        }
     }
 
     private class Node {
--- DSA/Workshop 2/Skeleton/src/com/company/dsa/Main.java
+++ DSA/Workshop 2/Skeleton/src/com/company/dsa/Main.java
@@ -1,7 +1,18 @@
 package com.company.dsa;
 
+import java.util.Arrays;
+import java.util.Iterator;
+
 public class Main {
     public static void main(String[] args) {
+        LinkedList<Integer> list = new LinkedList<>(Arrays.asList(1, 2, 3, 4, 5));
+
+        Iterator<Integer> iterator = list.iterator();
+
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next());
+        }
+
 
     }
 }
