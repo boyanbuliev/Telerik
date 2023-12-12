a6c8297344cfe3b2ec84126f2533b4691e5ff899
Boyan Buliev <boyanbot@gmail.com>
2023-12-12 14:29:59 +0200

    submitted solution for Cipher task

--- DSA/RecursionTasks3/src/Cipher.java
+++ DSA/RecursionTasks3/src/Cipher.java
@@ -1,5 +1,46 @@
-public class Cipher {
-    public static void main(String[] args) {
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
+public class Cipher {
+    private static List<String> result = new ArrayList<>();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+        String message = reader.readLine();
+
+        String[] cipher = reader.readLine().split("(?=[A-Z])");
+
+        Map<String, String> codes = new HashMap<>();
+        for (String str : cipher) {
+            codes.put(str.substring(0, 1), str.substring(1));
+        }
+        decipher(codes, new StringBuilder(), message);
+        System.out.println(result.size());
+            for (String str : result) {
+                System.out.println(str);
+
+        }
+    }
+
+    private static void decipher(Map<String, String> codes, StringBuilder curr, String message) {
+        if (message.length() == 0) {
+            result.add(curr.toString());
+            return;
+
+        }
+        for (Map.Entry<String, String> entry : codes.entrySet()) {
+            if (message.startsWith(entry.getValue())) {
+                curr.append(entry.getKey());
+                decipher(codes, curr, message.replaceFirst(entry.getValue(), ""));
+                curr.deleteCharAt(curr.length() - 1);
+            }
+        }
     }
 }
+
