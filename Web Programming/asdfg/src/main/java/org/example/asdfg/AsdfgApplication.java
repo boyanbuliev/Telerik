4bf0201e60d594fab7772ce72006e6733b0d9de5
Boyan Buliev <boyanbot@gmail.com>
2024-01-03 18:12:05 +0200

    new project for test

---
+++ Web Programming/asdfg/src/main/java/org/example/asdfg/AsdfgApplication.java
@@ -0,0 +1,13 @@
+package org.example.asdfg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AsdfgApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AsdfgApplication.class, args);
+    }
+
+}
---
+++ Web Programming/asdfg/src/test/java/org/example/asdfg/AsdfgApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.asdfg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AsdfgApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
