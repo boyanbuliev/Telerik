36f8728d4c8e4a0aa4c3572a21630df5719884da
Boyan Buliev <74222818+boyanbuliev@users.noreply.github.com>
2024-01-03 11:19:06 +0200

    Add files via upload

---
+++ BeerTag/src/test/java/org/telerik/web/beertag/BeerTagApplicationTests.java
@@ -0,0 +1,13 @@
+package org.telerik.web.beertag;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BeerTagApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
