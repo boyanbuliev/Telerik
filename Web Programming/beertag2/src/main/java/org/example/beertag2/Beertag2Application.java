3daa7b1aa32e8ab3eb59974e9a689a45efd933f4
Boyan Buliev <boyanbot@gmail.com>
2024-01-03 18:10:00 +0200

    commit message

---
+++ Web Programming/beertag2/src/main/java/org/example/beertag2/Beertag2Application.java
@@ -0,0 +1,13 @@
+package org.example.beertag2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Beertag2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Beertag2Application.class, args);
+    }
+
+}
---
+++ Web Programming/beertag2/src/test/java/org/example/beertag2/Beertag2ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.beertag2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Beertag2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
